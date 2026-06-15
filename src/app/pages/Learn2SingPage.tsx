154dfe83689ac06acc255e3cc8f18e417eed7ec4
Mindya Biznazz <mindyabiznazz@Mindyas-MacBook-Pro.local>
2026-06-15 11:14:19 +0800

    Auto-commit: update Learn2SingPage.tsx

--- src/app/pages/Learn2SingPage.tsx
+++ src/app/pages/Learn2SingPage.tsx
@@ -243,7 +243,8 @@ const l2sPrograms = [
     urgency: null,
   },
   {
-    icon: <img src={blueprintThumbImg} alt="Exercise 3" className="w-full h-full object-cover rounded-2xl" />,
+    icon: <Trophy size={28} />,
+    image: blueprintThumbImg,
     tag: "Most Requested",
     tagColor: "bg-[#dcfce7] text-[#166534]",
     title: "The 90-Day Keynote Authority Blueprint",
